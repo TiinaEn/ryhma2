ce5a1f73b9bd5230f8d46e0375ff3d1ee0e4492e
nikisem <niki.semenkov@gmail.com>
2018-03-13 16:12:55 +0200

    Korjattu kontrolleria. Nyt kirjautunut pystyy postaamaan useita viestejä.

--- src/main/java/fi/academy/forum/Kontrolleri.java
+++ src/main/java/fi/academy/forum/Kontrolleri.java
@@ -31,7 +31,7 @@ public class Kontrolleri {
         viesti.setKayttaja(kirjautunut.get());
         repo.save(viesti);
         model.addAttribute("viestit", repo.findAll());
-        model.addAttribute("lisattava", new Viesti());
+        model.addAttribute("lisattava", new Viesti(kirjautunut.get()));
 
         return "index";
 
