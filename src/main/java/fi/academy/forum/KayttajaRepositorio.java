50be02e3cb05eb7a0799bbd75fa31178d7346880
Elina Rauhala <elina0rauhala@gmail.com>
2018-03-13 11:56:49 +0200

    Repot lisätty

---
+++ src/main/java/fi/academy/forum/KayttajaRepositorio.java
@@ -0,0 +1,7 @@
+package fi.academy.forum;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface KayttajaRepositorio extends CrudRepository<Kayttaja, Integer> {
+
+}
---
+++ src/main/java/fi/academy/forum/ViestiRepositorio.java
@@ -0,0 +1,7 @@
+package fi.academy.forum;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ViestiRepositorio extends CrudRepository<Viesti, Integer> {
+
+}
