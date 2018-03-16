ac57621c6becfa6545628dc372b93d473aa70ef8
Renne Brandt <renne.brandt@iki.fi>
2018-03-16 16:09:20 +0200

    joo

--- src/main/java/fi/academy/forum/Kontrolleri.java
+++ src/main/java/fi/academy/forum/Kontrolleri.java
@@ -215,12 +215,12 @@ public class Kontrolleri {
         k.setViestienMaara(k.getViestienMaara()-1);
         repo.deleteById(viesti.getId());
 
-        List<Viesti> viestilista = repo.etsiKaikkiAikajarjestyksessa();
+        List<Viesti> viestilista = repo.etsiKaikkiViestiketjut();
         model.addAttribute("viestit", viestilista);
         model.addAttribute("lisattava", new Viesti(k));
         model.addAttribute("kirjautunut", k);
         model.addAttribute("admin", k.getAdminoikeus());
-        return "index";
+        return "viestiketjut";
     }
 
     @GetMapping("/logout")
